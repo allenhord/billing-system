0a44a4b3a2e49d5bd59dc6c6d14f1bca91879f0c
howlinworf <allen.t.hord@gmail.com>
2019-02-07 21:01:14 -0500

    Uploading initial files

---
+++ doctorTypeImp.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+#include <string>
+#include "doctorType.h"
+
+using namespace std;
+
+void doctorType::print() const
+{
+	personType::print();
+	cout << ": " << speciality;
+}
+
+void doctorType::setSpeciality(string spl)
+{
+	speciality = spl;
+}
+
+string doctorType::getSpeciality()
+{
+	return speciality;
+}
+
+//constructor
+doctorType::doctorType(string first, string last, string spl) : personType(first, last)
+{
+	speciality = spl;
+}
