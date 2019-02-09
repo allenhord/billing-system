605e7c3b83ef5dac2b1b0de2bae7c8151354d59b
howlinworf <allen.t.hord@gmail.com>
2019-02-09 14:27:32 -0500

    Updated patientTypeImp.cpp, added simple test program for patientType
    -patientType constructor, and aggregate set and print functions completed
    -smaller functions may not be used/needed?
    -simple test program added

--- patientTypeImp.cpp
+++ patientTypeImp.cpp
@@ -9,11 +9,15 @@ using namespace std;
 void patientType::print() const
 {
 	personType::print();
-	cout << "\nID: " << ID <<
-		"\nDOB: " << dateOfBirth <<
-		"\nPhysician: " << attendingPhysician <<
-		"\nAdmit Date: " << admitDate <<
-		"\nDischarge Date: " << dischargeDate;
+	cout << "\nID: " << ID << "\nDOB: ";
+	dateOfBirth.printDate();
+	cout << "\nPhysician: ";
+	attendingPhysician.print();
+	cout << "\nAdmit Date: ";
+	admitDate.printDate();
+	cout << "\nDischarge Date: ";
+	dischargeDate.printDate();
+	cout << endl;
 }
 
 void patientType::setInfo(string id, string fName, string lName,
@@ -23,7 +27,28 @@ void patientType::setInfo(string id, string fName, string lName,
 {
 	personType::setName(fName, lName);
 	ID = id;
-	//set DOB will go here
-	attendingPhysician = docFrName + " " + docLaName;
-	//TO BE CONTINUED
+	dateOfBirth.setDate(bMth, bDay, bYear);
+	attendingPhysician = doctorType(docFrName, docLaName, docSpl);
+	admitDate.setDate(admMth, admDay, admYear);
+	dischargeDate.setDate(disChMth, disChDay, disChYear);
 }
+
+patientType::patientType(string id, string fName, string lName,
+	int bDay, int bMth, int bYear,
+	string docFrName, string docLaName, string docSpl,
+	int admDay, int admMth, int admYear,
+	int disChDay, int disChMth, int disChYear)
+	: personType(fName, lName),
+	dateOfBirth(bMth, bDay, bYear),
+	attendingPhysician(docFrName, docLaName, docSpl),
+	admitDate(admMth, admDay, admYear),
+	dischargeDate(disChMth, disChDay, disChYear)
+{
+	ID = id;
+}
+
+//string ID;
+//dateType dateOfBirth;
+//doctorType attendingPhysician;
+//dateType admitDate;
+//dateType dischargeDate;
