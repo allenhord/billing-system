0b85f5374a03e627844283e72bb234b89c8175f0
howlinworf <allen.t.hord@gmail.com>
2019-02-13 10:25:05 -0500

    Comments added

--- dateTypeImp.cpp
+++ dateTypeImp.cpp
@@ -1,10 +1,11 @@
-//Implementation file date
+//Implementation file for class dateType
    
 #include <iostream>
 #include "dateType.h"
 
 using namespace std;
 
+//Function to set the date
 void dateType::setDate(int month, int day, int year)
 {
     dMonth = month;
@@ -12,27 +13,31 @@ void dateType::setDate(int month, int day, int year)
     dYear = year;
 }
 
+//Function to return the day
 int dateType::getDay() const 
 {
     return dDay;
 }
 
+//Function to return the month
 int dateType::getMonth() const 
 {
     return dMonth;
 }
 
+//Function to return the year
 int dateType::getYear() const 
 {
     return dYear;
 }
 
+//function to print the date
 void dateType::printDate() const
 {
     cout << dMonth << "-" << dDay << "-" << dYear;
 }
 
-    //Constructor with parameters
+//Constructor with parameters
 dateType::dateType(int month, int day, int year) 
 { 
     dMonth = month;
