b454437aeafb32f3062448527e906da47888b4d6
howlinworf <allen.t.hord@gmail.com>
2019-02-13 10:29:05 -0500

    Added comments

--- doctorType.h
+++ doctorType.h
@@ -1,4 +1,4 @@
- 
+//header file for class doctorType 
 #ifndef H_doctorType
 #define H_doctorType
 
@@ -7,16 +7,24 @@
  
 using namespace std;
 
+//definitions for class doctorType
 class doctorType: public personType
 {
 public:
+    //function to print doctor name and speciality
     void print() const;
+    
+    //function to set speciality
     void setSpeciality(string spl);
+    
+    //function to get speciality
     string getSpeciality();
- 
+    
+    //constructor for doctorType with default parameters
     doctorType(string first = "", string last = "", string spl = "");
 
  private:
+    //variable to store speciality
     string speciality; 
 };
 
