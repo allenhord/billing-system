20dde64363d0d0dbf85dd0404a960513a4c6ea2d
howlinworf <allen.t.hord@gmail.com>
2019-02-13 14:40:42 -0500

    Added comments

--- doctorTypeImp.cpp
+++ doctorTypeImp.cpp
@@ -1,26 +1,30 @@
+//Implementation file for class dateType
 #include <iostream> 
 #include <string>
 #include "doctorType.h"
 
 using namespace std;
 
+//function to print doctor name and speciality
 void doctorType::print() const
 {
 	personType::print();
 	cout << ": " << speciality;
 }
 
+//function to set speciality
 void doctorType::setSpeciality(string spl)
 {
 	speciality = spl;
 }
 
+//function to get speciality
 string doctorType::getSpeciality()
 {
 	return speciality;
 }
 
-//constructor
+//constructor for class doctorType
 doctorType::doctorType(string first, string last, string spl) : personType(first, last)
 {
 	speciality = spl;
